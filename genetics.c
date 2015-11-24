0e2930d40fed856dc0d9208d5ab02da4fada27df
Tech0verlord <elvin1396@gmail.com>
2015-11-24 12:02:56 -0500

    Initial Upload
    Gotta start somewhere

---
+++ genetics.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void determineScenario(int numCases);
+char NUMtoDNA();
+int DNAtoNUM();
+
+int main(){
+
+    int cases;
+
+    scanf("%d",&cases);
+    determineScenario(cases);
+
+
+
+
+
+    system("pause");
+    return 0;
+};
+
+void determineScenario(int numCases){
+
+    int i;
+
+
+    for(i=0;i<numCases;i++){
+
+    }
+
+}
