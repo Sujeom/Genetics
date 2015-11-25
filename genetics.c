4b3d097a61aa6b02c04ba0dc4e2d61f7f8ee72bf
Tech0verlord <elvin1396@gmail.com>
2015-11-25 18:52:49 -0500

    Finished NUMtoDNA & print function
    Finished and verified that NUMtoDNA is properly decrypting and
    translating. Print function created and finalized as well.
    
    Next: DNAtoNUM

--- genetics.c
+++ genetics.c
@@ -7,7 +7,7 @@ void determineScenario(int numCases); //Reads in each test case (of up to LENGTH
 char NUMtoDNA(int nums); //Convert number into DNA code value
 int DNAtoNUM(char* letters); //"Encrypt" DNA code value into numbers
 int findArrayLength(char* array); // Finds the Length of the array. NUMtoDNA: to specify length of temporary array to swap ints for correct decryption order. DNAtoNUM: setting highest power.
-void printSequence(char* output);
+void printSequence(char* output, int arrayLength);
 
 #define LENGTH 15
 #define A 0
@@ -61,6 +61,8 @@ char NUMtoDNA(int nums){
     int arrayLength;
     char sequence[LENGTH];
     int i = 0;
+    int k = 0;
+    int j;
 
     printf("Sequence before memset: %s\n", sequence);
     memset(&sequence, 0, sizeof(sequence));
@@ -76,7 +78,36 @@ char NUMtoDNA(int nums){
     }
 
     arrayLength = findArrayLength(sequence);
+    char sequenceCoded[arrayLength];
     printf("Sequence Length: %d\n", arrayLength);
+
+    for(j=arrayLength-1;j>=0;j--){
+        printf("K: %d\n", k);
+        if(sequence[j] == '0') {
+            sequenceCoded[k] = 'A';
+            printf("HIT\n");
+            k++;
+        }
+        else if(sequence[j] == '1') {
+            sequenceCoded[k] = 'C';
+            printf("HIT\n");
+            k++;
+        }
+        else if(sequence[j] == '2') {
+            sequenceCoded[k] = 'G';
+            printf("HIT\n");
+            k++;
+        }
+        else if(sequence[j] == '3') {
+            sequenceCoded[k] = 'T';
+            printf("HIT\n");
+            k++;
+        }
+    }
+
+    printf("Coded Sequence: %s (FINAL DIGIT IS TRASH)\n", sequenceCoded); //ignore any ending characters that aren't 'C','A','G', or 'T', remaining trash characters that don't get printed.
+
+    printSequence(sequenceCoded, arrayLength);
 
 
     return 0;
@@ -97,5 +128,18 @@ int findArrayLength(char* array){
         count++;
     }
     return count;
+
+}
+
+void printSequence(char* output, int arrayLength){
+
+    int i;
+
+    printf("Sequence #%d: ",sequenceNUM);
+    sequenceNUM++;
+    for(i=0;i<arrayLength;i++){
+        printf("%c", output[i]);
+    }
+    printf("\n");
 
 }
