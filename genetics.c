95f695c59670de89914e5f4c0921d5aa53a8123b
Tech0verlord <elvin1396@gmail.com>
2015-11-24 22:16:19 -0500

    Finished determination, set up basic functions
    Completed the function that determines which function to use, DNAtoNUM,
    or NUMtoDNA. Added basic print statements to make sure data was being
    sent to said functions properly.  Comments to delcarations. Changed
    length of array to a #define-d value, for easy changes when necessary

--- genetics.c
+++ genetics.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-void determineScenario(int numCases);
-char NUMtoDNA();
-int DNAtoNUM(char nums);
+void determineScenario(int numCases); //Reads in each test case (of up to LENGTH characters), and determines which of the two functions below are used.
+char NUMtoDNA(char* nums); //Convert number into DNA code value
+int DNAtoNUM(char* letters); //"Encrypt" DNA code value into numbers
+
+#define LENGTH 15
+
+
 
 int main(){
 
@@ -25,22 +29,34 @@ void determineScenario(int numCases){
 
     int i;
     int atoiVAL;
-    char input[15];
+    char input[LENGTH];
 
 
     for(i=0;i<numCases;i++){
+       // printf("LOOP: %d\n",i);
         memset(&input, 0, sizeof(input));
         scanf("%s",input);
         atoiVAL = atoi(input);
         if(atoiVAL !=0){
             NUMtoDNA(input);
         }
+        else if(atoiVAL == 0){
+            DNAtoNUM(input);
+        }
     }
 
 }
 
-char NUMtoDNA(char nums){
+char NUMtoDNA(char* nums){
 
+    printf("NUM to DNA (nums value): %s\n", nums);
+    return 0;
 
+}
+
+int DNAtoNUM(char* letters){
+
+    printf("DNA to NUM (letters value): %s\n", letters);
+    return 0;
 
 }
